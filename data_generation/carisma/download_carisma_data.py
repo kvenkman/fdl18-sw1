1a68dfb4b149002b259e29969abc759a810b8523
Karthik <karthikvt@vt.edu>
2019-11-16 18:07:25 -0700

    Bug fixes and improvements

--- data_generation/carisma/download_carisma_data.py
+++ data_generation/carisma/download_carisma_data.py
@@ -24,18 +24,24 @@ def download_carisma_data(year, station_name):
     # Setting up directory structure
     data_root = os.path.join(root_folder, 'data')
     l1_data = os.path.join(data_root, 'level1')
-    this_year_folder = os.path.join(l1_data, str(year))
+    station_folder = os.path.join(l1_data, station_name)
+    this_year_folder = os.path.join(station_folder, str(year))
 
     if not os.path.exists(data_root):
         os.mkdir(data_root)
         
     if not os.path.exists(l1_data):
         os.mkdir(l1_data)
-    
+
+    if not os.path.exists(station_folder):
+        os.mkdir(station_folder)
+     
     if not os.path.exists(this_year_folder):
         os.mkdir(this_year_folder)
     
     months = range(1, 13)
+    error_count = 0
+    success_count = 0
 
     for month in months:
         month_str = str(month).zfill(2) # Left pad with zeros
@@ -43,7 +49,7 @@ def download_carisma_data(year, station_name):
         days = monthrange(year, month)
         days = days[1] # Month range returns first weekday, and ndays(month)stat
         
-        data_output_path = os.path.join(l1_data, str(year), month_str)
+        data_output_path = os.path.join(this_year_folder, month_str)
         
         if not os.path.exists(data_output_path):
             os.mkdir(data_output_path)
@@ -56,13 +62,14 @@ def download_carisma_data(year, station_name):
             fetch_url = carisma_data_url + str(year) + '/' + \
                         month_str + '/' + day_str + '/' + this_filename
 
-            check = os.system("wget -q " + fetch_url + " " + os.path.join(data_output_path, this_filename))
+            if not os.path.exists(os.path.join(data_output_path, this_filename[:-3])): # If file does not already exist
+                check = os.system("wget -q " + fetch_url + " -O " + os.path.join(data_output_path, this_filename))
             
-            # Check = 0 for successful download
-            if not check:
-                success_count += 1
-                os.system("gunzip -qf " + this_filename)
-            else:
-                error_count += 1
+                # Check = 0 for successful download
+                if not check:
+                    success_count += 1
+                    os.system("gunzip -qf " + os.path.join(data_output_path, this_filename))
+                else:
+                    error_count += 1
                 
     print("Attempted downloads: {}, Successful downloads: {}.".format(error_count + success_count, success_count))
