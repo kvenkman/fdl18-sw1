6a42ac1c6ab0b2f357c07006bbb302fbdba1f778
Karthik <karthikvt@vt.edu>
2019-12-27 22:32:00 -0700

    Added comments

--- data_generation/carisma/download_carisma_data.py
+++ data_generation/carisma/download_carisma_data.py
@@ -14,7 +14,42 @@ import os
 from calendar import monthrange
 
 def download_carisma_data(year, station_name):
+    """
+    Function that downloads magnetometer data from the CARISMA network
+    To generate the level 1 dataset, we download the 1 Hz data.
+      
+    The website on which the data is hosted (http://data.carisma.ca/) uses the 
+    folder structure : {data_type}/{cadence}/{year}/{month}/{day}/{station_files}
+    
+    To download data for a given station, for a given year, the script iterates 
+    over the month and day numbers (accounting for leap days) and queries for 
+    the available files and downloads them.
+    
+    The files on the website are stored in as a compressed archive. The script
+    extracts the files and deletes the downloaded archives.
+    
+    Inputs
+    ----------
+    year
+        the year for which data is to be downloaded (the CARISMA dataset
+        spans from Dec 2013 - present)
+        
+    station_name
+        A list of valid station names are provided in the 'carisma_station_list'
+        document
 
+    Output
+    -----
+    The script downloads the magnetometer data archives and extracts the text
+    readable files which have the extension ".F01". The script generates a new 
+    folder structure of the format:
+    ../level1/{station_name}/{year}/{month}/{station_files}
+        
+    Example
+    --------
+    >>> download_carisma_data(2015, "mcmu")
+    """
+    
     # CARISMA data available here
     carisma_data_url = 'http://data.carisma.ca/FGM/1Hz/'
     
